6b108ae883134a033775c6295e4c70d3e7fa44c4
keananpierre1234 <keananpierrekj@hotmail.com>
2015-12-17 22:13:30 +0000

    i got the three point shot working

--- basketball.js
+++ basketball.js
@@ -1,3 +1,28 @@
 var promptForPlayerName = function (playerNumber) {
     var name = prompt(playerNumber + " what is your name?");
+
+
+return name;
+    
 };
+
+var tryTwoPointShot = function() {
+    if (Math.random() >= 0.2 && Math.random() >= 0.2 ) {
+        return true;   
+    } else { 
+        return false;
+    } 
+};
+
+var getShotString = function(playerName, points, wasSuccessful) {
+    return playerName + " shot a three ";
+    
+};
+
+var tryThreePointShot = function() {
+    return true
+};
+
+var updateScore = function(wasShotMade, currentScore, points){
+    return true;
+}
